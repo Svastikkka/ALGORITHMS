773764f68d9828479bfa92bc41bdff2cb8076884
Svastikkka <ManshuSharma143Repo@gmail.com>
2020-09-10 22:58:26 +0530

    10 SEP 2020 BUBBLE SORT

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,1 +1,9 @@
+def BubleSort(arr):
+    for i in range(len(arr)-1):
+        for j in range(len(arr)-i-1): 
+            if arr[j]>arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+        
 arr=list(map(int,input().split()))
+print(BubleSort(arr))
